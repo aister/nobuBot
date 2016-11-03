a94e989f696a20f86caca2be8ace10f7f689763c
aister <hellodecasino@gmail.com>
2016-11-03 20:36:35 +0700

    added clean command for admins

---
+++ commands/Admin/clean.js
@@ -0,0 +1,17 @@
+exports.help = "clean <number> :: Clear xx messages";
+exports.exec = (bot, message, msgArray, callback) => {
+  if (!message.guild) return;
+  if (message.member.highestRole.name.toLowerCase().includes('admin')) {
+    if (!isNaN(msgArray[1])) {
+      (function deleteMessages(number) {
+        if (number > 100) {
+          message.channel.bulkDelete(100).then(() => {
+            setTimeout(function() { deleteMessages(number - 100); }, 1000);
+          });
+        } else {
+          message.channel.bulkDelete(number).then(callback);
+        }
+      })(msgArray[1]);
+    }
+  } else message.reply("only admins can use this command");
+}

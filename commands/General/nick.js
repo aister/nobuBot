e489be90c05fb33150284ccdd84bb2c3c0e5cfb4
aister <hellodecasino@gmail.com>
2016-11-09 19:18:30 +0700

    update `nick`

--- commands/General/nick.js
+++ commands/General/nick.js
@@ -5,13 +5,18 @@ exports.exec = (client, message, msgArray, callback) => {
     if (mention.size > 0) {
       mention = mention.first();
       name = "";
-      if (msgArray.length > 2) name = msgArray[2];
+      if (msgArray.length > 2) name = msgArray.slice(2).join(' ');
       message.guild.member(mention.id).setNickname(name).then(() => {
         message.channel.sendMessage('Nickname changed');
         callback();
       });
     } else {
-      message.channel.sendMessage('Wrong syntax');
+      name = "";
+      if (msgArray.length > 1) name = msgArray.slice(1).join(' ');
+        message.member.setNickname(name).then(() => {
+        message.channel.sendMessage('Nickname changed');
+        callback();
+      });
     }
   } else {
     message.channel.sendMessage("You don't have the permission to change other's nickname");

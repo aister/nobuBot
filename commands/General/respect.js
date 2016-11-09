71afa76dd54d08359a7cf5ffe52c71226a1ea530
aister <hellodecasino@gmail.com>
2016-11-09 22:53:21 +0700

    'f'ix respect

--- commands/General/respect.js
+++ commands/General/respect.js
@@ -1,7 +1,7 @@
 exports.help = "respect :: F";
 exports.exec = (client, message, msgArray, callback) => {
   message.channel.sendMessage('Press F to pay respect').then(msg => {
-    msg.chanel.awaitMessages(m => m.content.toLowerCase() == "f", {time: 10000}).then(respects => { 
+    msg.channel.awaitMessages(m => m.content.toLowerCase() == "f", {time: 10000}).then(respects => { 
       message.channel.sendMessage(respects.size + " respects have been sent");
     });
   });

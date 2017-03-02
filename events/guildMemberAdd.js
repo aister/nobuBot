8ec6f4b819d5e209e1d0d3de1bfe17d683b278c8
aister <hellodecasino@gmail.com>
2017-03-02 17:35:05 +0700

    fix bugs

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,5 +1,12 @@
 exports.exec = (client) => {
   return (member) => {
-    if (!client.config.selfbot) member.guild.defaultChannel.sendMessage('Welcome ' + member + ' to ' + member.guild + '! Enjoy your stay!');
+    if (!client.config.selfbot) {
+      member.guild.defaultChannel.sendMessage('Welcome ' + member + ' to ' + member.guild + '! Enjoy your stay!');
+      if (member.guild.id == "274433246299684864") {
+        member.guild.channels.get("286680607260672000").fetchMessage("286683584637435914").then(m => {
+          member.sendMessage("Welcome " + member + " to " + member.guild.name + "\n\n" + m.content);
+        });
+      }
+    }
   }
 }

d1436bf7e8b3c8017c548dbbc49b8bff6111d0ac
aister <hellodecasino@gmail.com>
2017-03-02 17:47:14 +0700

    add playing status

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,7 @@ exports.exec = (client) => {
     taken = Date.now() - client.initTime;
     console.log("Nobu! Took " + taken + "ms to log in");
     client.initTime = taken + client.initTime;
+    client.user.setGame("with " client.prefix + "help command");
     if (client.config.logChannel && (channel = client.bot.channels.get(client.config.logChannel))) channel.sendMessage("Bot is running. " + process.env.HEROKU_RELEASE_VERSION || "");
   };
 }

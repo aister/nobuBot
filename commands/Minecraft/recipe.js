a199bace5f909102aad42cf7d3ebf6b2ef348024
aister <hellodecasino@gmail.com>
2017-03-04 12:45:00 +0700

    change recipe

--- commands/Minecraft/recipe.js
+++ commands/Minecraft/recipe.js
@@ -1,8 +1,17 @@
-recipes = require('../../mc.json');
+request = require('request');
 exports.help = "recipe <item> :: get the crafting recipe of the item";
 exports.exec = (bot, message, msgArray, callback) => {
-  msgArray = msgArray.slice(1).join(' ');
-  if (msgArray in recipes) {
-    message.channel.sendMessage('', {embed: { image: { url: recipes[msgArray]}}});
-  }
+  request('https://www.minecraftcraftingguide.net', function (err, res, body) {
+    body = body.slice(0, body.indexOf('id="Basic-Recipes'));
+    body = body.split("<div class='tooltip'>").slice(1);
+    msgArray = new RegExp(msgArray.slice(1).join(" "), "gi");
+    temp = "";
+    body.forEach(item => {
+      if (item.match(/<strong>[^<]+/)[0].slice(8).match(msgArray)) {
+        temp = "http:" + item.match(/src='[^']+/)[0].slice(5);
+      }
+    });
+    if (temp) message.channel.sendFile(temp);
+    else message.channel.sendMessage("Not found");
+  })
 }

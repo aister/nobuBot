91839a01b88a188d3506106c98b025509c4da60c
aister <hellodecasino@gmail.com>
2017-03-12 02:45:33 +0700

    add cooldown

--- commands/Valkyrie Crusade/vc-waifu.js
+++ commands/Valkyrie Crusade/vc-waifu.js
@@ -1,24 +1,33 @@
 var request = require('request');
 exports.help = "vc-waifu :: Marry a random maiden in Valkyrie Crusade";
+let cooldown = {};
 exports.exec = (bot, message, msgArray, callback) => {
-  msgArg = "http://aister.site90.com/api.php?mode=rand&db=vc&c=rarity&query=";
-  let chance = Math.random();
-  if (chance <= 0.05) msgArg += "LR";
-  else if (chance <= 0.15) msgArg += "UR";
-  else if (chance <= 0.5) msgArg += "SR";
-  else msgArg += "N";
-  request({ url: msgArg, json: true, followRedirect: false }, function(err, res, result) {
-    if (res.statusCode != 302 && result.item) {
-      body = result.item;
-      embed = {
-        title: "Congratulation!!",
-        color: 0xff0000,
-        description: "\u200b\nCongratulation! " + message.author + " have married to " + body.name + "! She has a rarity of " + body.rarity + ", how lucky!",
-        image: {
-          url: body.image
+  let time = 0;
+  if (!cooldown[message.author.id]) cooldown[message.author.id] = message.createdTimestamp;
+  else time = message.createdTimestamp - cooldown[message.author.id] - 900000;
+  if (time < 0) {
+    message.channel.sendMessage("You can only use this command once every 15 minutes. You can use it again in " + Math.ceil( - time / 60000) + " minutes");
+  } else {
+    cooldown[message.author.id] = message.createdTimestamp;
+    msgArg = "http://aister.site90.com/api.php?mode=rand&db=vc&c=rarity&query=";
+    let chance = Math.random();
+    if (chance <= 0.015) msgArg += "LR";
+    else if (chance <= 0.1) msgArg += "UR";
+    else if (chance <= 0.4) msgArg += "SR";
+    else msgArg += "N";
+    request({ url: msgArg, json: true, followRedirect: false }, function(err, res, result) {
+      if (res.statusCode != 302 && result.item) {
+        body = result.item;
+        embed = {
+          title: "Congratulation!!",
+          color: 0xff0000,
+          description: "\u200b\nCongratulation! " + message.author + " have married to " + body.name + "! She has a rarity of " + body.rarity + ", how lucky!",
+          image: {
+            url: body.image
+          }
         }
-      }
-      message.channel.sendMessage('', {embed}).then(callback).catch(console.log);
-    } else message.channel.sendMessage("Not found").then(callback);
-  });
+        message.channel.sendMessage('', {embed}).then(callback).catch(console.log);
+      } else message.channel.sendMessage("Not found").then(callback);
+    });
+  }
 }
--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -3,7 +3,7 @@ exports.exec = (client) => {
     if (!client.config.selfbot) {
       member.guild.defaultChannel.sendMessage('Welcome ' + member + ' to ' + member.guild + '! Enjoy your stay!');
       if (member.guild.id == "274433246299684864") {
-        member.guild.channels.get("286680607260672000").fetchMessage("286683584637435914").then(m => {
+        member.guild.channels.get("286680607260672000").fetchMessage("290206770293374986").then(m => {
           member.sendMessage("Welcome " + member + " to " + member.guild.name + "\n\n" + m.content);
         });
       }

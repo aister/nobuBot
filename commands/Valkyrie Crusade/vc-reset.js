b34a9d622fa3df64c98c57ba113b940e887ef458
aister <hellodecasino@gmail.com>
2017-05-09 10:58:26 +0700

    vc-reset

---
+++ commands/Valkyrie Crusade/vc-reset.js
@@ -0,0 +1,8 @@
+exports.help = "vc-reset :: Reset your nickname";
+exports.exec = (bot, message, msgArray, callback) => {
+  if (!message.guild.member(bot.bot.user.id).hasPermission('MANAGE_NICKNAMES')) {
+    message.channel.sendMessage("This command can't be used, because the bot can't change your nickname");
+    return;
+  }
+  message.member.setNickname("").then(() => { message.channel.send("You reseted your nickname, boohoo"); });
+}

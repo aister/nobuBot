c663bfbd8f034a57ba63dc50736e28d0bade9166
aister <hellodecasino@gmail.com>
2017-05-12 07:26:44 +0700

    add gamepedia

---
+++ commands/Gamepedia/vindictus.js
@@ -0,0 +1,18 @@
+var request = require('request');
+exports.help = "vindictus <search term> :: Search in Vindictus Gamepedia";
+exports.exec = (bot, message, msgArray, callback) => {
+  if (msgArray.length > 1) {
+    msgArg = msgArray.slice(1).join(' ');
+    msgArg = "http://vindictus.gamepedia.com/api.php?action=query&format=json&list=search&srprop=snippet&titles=&srsearch=" + encodeURI(msgArg);
+    message.channel.sendMessage('Searching...').then(msg => {
+      request({ url: msgArg, json: true, followRedirect: false }, function(err, res, body) {
+        if (res.statusCode != 302 && body.query && body.query.search && body.query.search.length) {
+          msg.edit(body.query.search[0].snippet.replace(/<[^>]+>/g, "") + "\n\nhttp://vindictus.gamepedia.com/" + encodeURI(body.query.search[0].title)).then(callback);
+          
+        } else msg.edit("Not found").then(callback);
+      });
+    });
+  } else {
+    message.channel.sendMessage('Wrong syntax');
+  }
+}

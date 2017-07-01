7e73e1177117d4f74850cb1c6a2e138ead034453
aister <hellodecasino@gmail.com>
2017-07-01 18:16:07 +0700

    Update greet.js

--- commands/Fate Grand Order/greet.js
+++ commands/Fate Grand Order/greet.js
@@ -1,5 +1,5 @@
 let greetings = "http://i.imgur.com/eoWffyo.png";
 exports.help = "greet :: Whassup!!";
 exports.exec = (client, message, msgArray, callback) => {
-  message.channel.send('', {file: {attachment: 'greetings', name: 'Whassup.png'}});
+  message.channel.send('', {file: {attachment: greetings, name: 'Whassup.png'}});
 }

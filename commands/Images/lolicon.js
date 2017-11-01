1d2699d091acfe18fd5872249bbd1a373de5eab7
aister <hellodecasino@gmail.com>
2017-11-02 00:11:21 +0700

    Update lolicon.js

--- commands/Images/lolicon.js
+++ commands/Images/lolicon.js
@@ -8,7 +8,7 @@ exports.exec = (bot, message, msgArray, callback) => {
     img_bg.onload = function () {
       ctx.drawImage(img_bg, 0, 0, 617, 319);
       ctx.font = "bold 30px Arial";
-      var words = message.member.displayName;
+      var words = message.author.username;
       var metrics = ctx.measureText(words);
       ctx.fillText(words, 300 - metrics.width / 2, 150);
       message.channel.send("", {file: {attachment:canvas.toBuffer()}});

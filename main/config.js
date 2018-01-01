3347d4ebaeb58e793518de81764026f11a0e739d
Aister <hellodecasino@gmail.com>
2018-01-01 15:19:22 +0700

    I knew I would mess up somewhere

--- main/config.js
+++ main/config.js
@@ -1,5 +1,6 @@
 module.exports = class Config {
   constructor(option) {
+    option = option || {};
     this.prefix = option.prefix || "$";
     this.selfbot = option.selfbot || false;
     this.ownerID = option.ownerID || "184369428002111488";

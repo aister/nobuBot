1534d047b1616a1b476e6124fe0f93aecda1b549
aister <hellodecasino@gmail.com>
2018-01-16 15:05:22 +0700

    test

--- commands/Fate Grand Order/us-profile.js
+++ commands/Fate Grand Order/us-profile.js
@@ -19,10 +19,10 @@ module.exports = class FGOUSProfileCommand extends Command {
   run(message, args, prefix) {
     let player = message.author.id;
     if (args = args.join(' ')) player = args.match(/(?:<@!?)?(\d+)/)[1];
-    this.main.db.get(`fgoUSProfile_${player}`).then((profile) => {
-      if (profile) {
-        profile = JSON.parse(profile);
-        if (!profile.privacy || !args) message.channel.send('', {embed: this.main.util.fgoProfile(message.author, profile)});
+    Promise.all([this.main.db.get(`fgoUSProfile_${player}`), this.main.client.fetchUser(player)].then((profile) => {
+      if (profile[0]) {
+        profile[0] = JSON.parse(profile[0]);
+        if (!profile.privacy || !args) message.channel.send('', {embed: this.main.util.fgoProfile(profile[1], profile[0])});
         else message.channel.send(`This player has set his privacy setting to true, thus the profile cannot be displayed`);
       } else if (args) message.channel.send(`Cannot find profile of provided player. Please recheck your arguments and try again`);
       else message.channel.send(`Profile not found, please use \`${prefix}profile-edit\` to create one`);

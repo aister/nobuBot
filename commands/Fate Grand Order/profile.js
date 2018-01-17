26be829b5c17dab772bf6f97607cdb3159667962
aister <hellodecasino@gmail.com>
2018-01-17 15:02:02 +0700

    Update profile.js

--- commands/Fate Grand Order/profile.js
+++ commands/Fate Grand Order/profile.js
@@ -23,13 +23,14 @@ module.exports = class FGOProfileCommand extends Command {
       if (mentionID) player = mentionID[1];
       else player = "";
     }
-    Promise.all([this.main.db.get(`fgoUSProfile_${player}`), this.main.client.fetchUser(player)]).then((profile) => {
+    console.log(player);
+    Promise.all([this.main.db.get(`fgoProfile_${player}`), this.main.client.fetchUser(player)]).then((profile) => {
       if (profile[0]) {
         profile[0] = JSON.parse(profile[0]);
         if (!profile.privacy || !args) message.channel.send('', {embed: this.main.util.fgoProfile(profile[1], profile[0])});
         else message.channel.send(`This player has set his privacy setting to true, thus the profile cannot be displayed`);
       } else if (args) message.channel.send(`Cannot find profile of provided player. Please recheck your arguments and try again`);
       else message.channel.send(`Profile not found, please use \`${prefix}profile-edit\` to create one`);
-    });
+    }).catch(console.log);
   }
 }

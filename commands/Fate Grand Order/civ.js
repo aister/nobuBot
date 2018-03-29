e16a2b13ab507ede024f8b025cf50c25c74c9200
aister <hellodecasino@gmail.com>
2018-03-30 01:34:18 +0700

    Update civ.js

--- commands/Fate Grand Order/civ.js
+++ commands/Fate Grand Order/civ.js
@@ -15,7 +15,10 @@ module.exports = class CivilizationCommand extends Command {
     })
   }
   run(message, args, prefix) {
-    if (this.main.util.rand(0, 1) > 0.5) message.channel.send(`${args.displayName} is a good civilization!`);
-    else message.channel.send(`${args.displayName} is a bad civilization!`);
+    args = message.mentions.users.first() || message.author;
+    message.guild.fetchMember(args).then(i => {
+      if (this.main.util.rand(0, 1) > 0.5) message.channel.send(`${i.displayName} is a good civilization!`);
+      else message.channel.send(`${i.displayName} is a bad civilization!`);
+    });
   }
 }

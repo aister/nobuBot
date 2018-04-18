c27d267e02fcefa7bad54251505372f6a2453252
Aister <hellodecasino@gmail.com>
2018-04-18 08:36:33 +0700

    fix

--- dashboard/server.js
+++ dashboard/server.js
@@ -84,13 +84,13 @@ module.exports = class Dashboard {
     this.wss.on('connection', (ws, req) => {
       ws.on('error', () => {});
       ws.on('message', data => {
-        console.log(data.type);
+        data = JSON.parse(data);
         if (data.type == "cfyc") {
           snek.get('http://www.cfyc.com.vn/en/schedule').then(body => {
-            body = body.match(/https%3A%2F%2Fimages.cfyc.com.vn%2Fphn_image%2Fschedule%2F(Phu-Nhuan|Quan-3)-\d+\.jpg/g).map(i => {
+            body = body.text.match(/https%3A%2F%2Fimages.cfyc.com.vn%2Fphn_image%2Fschedule%2F(Phu-Nhuan|Quan-3)-\d+\.jpg/g).map(i => {
               return `<img src="${decodeURIComponent(i)}">`;
             }).join("<br>");
-            ws.send(body);
+            ws.send(JSON.stringify({type: "cfyc", img: body}));
           });
         }
       });
